7c627be0f48ed8b1568fb4c954401b8bffa0c2cf
AjnusAfter <darkskater360graus@gmail.com>
2026-03-22 01:00:45 -0300

    Problem E Solved

--- VJudge/Lista 1 - Introdução ao C++/E-mpp.cpp
+++ VJudge/Lista 1 - Introdução ao C++/E-mpp.cpp
@@ -7,6 +7,40 @@ typedef long long ll;
 
 int main()
 {
+    string s;
+    cin >> s;
+    map<char, int> frequencia;
+
+    for (char c : s)
+    {
+        frequencia[c]++;
+    }
+
+    vector<char> mais_freq;
+    ll max_count = 0;
+
+    for (auto &[k, v] : frequencia)
+    {
+        if (v > max_count)
+        {
+            max_count = v;
+        }
+    }
+
+    for (auto &[k, v] : frequencia)
+    {
+        if (v == max_count)
+        {
+            mais_freq.push_back(k);
+        }
+    }
+
+    for (char c : mais_freq)
+    {
+        s.erase(remove(s.begin(), s.end(), c), s.end());
+    }
+
+    cout << s << "\n";
 
     return 0;
 }
