c0644c9d448b8b1dfcc2cd3241b8beee7a1a9094
AjnusAfter <darkskater360graus@gmail.com>
2026-03-22 04:30:14 -0300

    Problem F Solved

--- VJudge/Lista 1 - Introdução ao C++/F-NearestSmallestValues.cpp
+++ VJudge/Lista 1 - Introdução ao C++/F-NearestSmallestValues.cpp
@@ -7,6 +7,33 @@ typedef long long ll;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll n;
+    cin >> n;
+
+    vector<ll> vec(n);
+
+    forn(i, 0, n)
+    {
+        cin >> vec[i];
+    }
+
+    stack<pair<ll, ll>> st;
+
+    forn(i, 0, n)
+    {
+        while (!st.empty() && st.top().first >= vec[i])
+            st.pop();
+
+        if (st.empty())
+            cout << 0 << " ";
+        else
+            cout << st.top().second << " ";
+
+        st.push({vec[i], i + 1});
+    }
 
     return 0;
 }
