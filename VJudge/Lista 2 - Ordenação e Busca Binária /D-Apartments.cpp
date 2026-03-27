a9c7b2bfb0732ecf311e552333ca8032a6124889
AjnusAfter <darkskater360graus@gmail.com>
2026-03-27 08:13:18 -0300

    D: Apartments solved

--- VJudge/Lista 2 - Ordenação e Busca Binária /D-Apartments.cpp
+++ VJudge/Lista 2 - Ordenação e Busca Binária /D-Apartments.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <set>
 using namespace std;
 
 typedef long long ll;
@@ -6,7 +7,42 @@ typedef long long ll;
 
 int main()
 {
-    
+    ll n, m, k;
+    cin >> n >> m >> k;
+
+    vector<ll> applicants(n);
+    forn(i, 0, n)
+    {
+        cin >> applicants[i];
+    }
+
+    multiset<ll> apartments;
+    forn(i, 0, m)
+    {
+        ll x;
+        cin >> x;
+        apartments.insert(x);
+    }
+
+    sort(applicants.begin(), applicants.end());
+
+    ll converted_applicants = 0;
+    forn(i, 0, n)
+    {
+        ll min_size_aceito = applicants[i] - k;
+        ll max_size_aceito = applicants[i] + k;
+
+        // menor apartamento >= min_size_aceito
+        auto it = apartments.lower_bound(min_size_aceito);
+
+        if (it != apartments.end() && *it <= max_size_aceito)
+        {
+            converted_applicants++;
+            apartments.erase(it); // remove ap alocado
+        }
+    }
+
+    cout << converted_applicants << "\n";
 
     return 0;
 }
