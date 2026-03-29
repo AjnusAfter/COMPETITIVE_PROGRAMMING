a9fbbcc4226840e4d7ab91d4d6b29d9cbd55556f
AjnusAfter <darkskater360graus@gmail.com>
2026-03-29 14:35:40 -0300

    set B: Subarray Sums II

---
+++ VJudge/Lista 3 - Greedy & Two Pointers /B-SubarraySumsII.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> v64;
+typedef pair<ll, ll> p64;
+
+#define forn(i, s, e) for (ll i = (s); i < (e); i++)
+
+int main()
+{
+
+    return 0;
+}
