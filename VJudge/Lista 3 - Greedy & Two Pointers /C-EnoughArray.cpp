9b27c51077d617a39aa956d955b41d3dfd519a9e
AjnusAfter <darkskater360graus@gmail.com>
2026-03-29 18:25:52 -0300

    set C: Enough Array

---
+++ VJudge/Lista 3 - Greedy & Two Pointers /C-EnoughArray.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vec64;
+typedef pair<ll, ll> p64;
+
+#define _                    \
+    ios::sync_with_stdio(0); \
+    cin.tie(0)
+#define forn (i, s, e) for (ll i = (s); i < (e); i++)
+#define ln "\n"
+
+int main()
+{
+
+    return 0;
+}
