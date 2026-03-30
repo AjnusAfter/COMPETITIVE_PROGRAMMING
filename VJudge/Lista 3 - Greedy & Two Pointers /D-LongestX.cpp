af615f416a2ecdbcfc695e28d75dcc22e0bcf187
AjnusAfter <darkskater360graus@gmail.com>
2026-03-29 21:58:07 -0300

    D: Longest X solved

--- VJudge/Lista 3 - Greedy & Two Pointers /D-LongestX.cpp
+++ VJudge/Lista 3 - Greedy & Two Pointers /D-LongestX.cpp
@@ -15,5 +15,36 @@ int main()
 {
     _;
 
+    string S;
+    ll K;
+    cin >> S >> K;
+
+    int N = S.size();
+    ll lp = 0;
+    ll dots = 0;
+    ll ans = 0;
+
+    forn(rp, 0, N)
+    {
+        if (S[rp] == '.')
+        {
+            dots++;
+        }
+
+        while (dots > K)
+        {
+            if (S[lp] == '.')
+            {
+                dots--;
+            }
+
+            lp++;
+        }
+
+        ans = max(ans, rp - lp + 1);
+    }
+
+    cout << ans << ln;
+
     return 0;
 }
