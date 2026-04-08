9aa644e0d0e317eb2f7ef7614cf67882a994276e
AjnusAfter <darkskater360graus@gmail.com>
2026-04-07 23:58:43 -0300

    Challenge Solved

--- Kattis/Challenge/Challenge.cpp
+++ Kattis/Challenge/Challenge.cpp
@@ -13,8 +13,20 @@ typedef pair<ll, ll> p64;
 
 int main()
 {
+
     _;
 
+    ll a, b;
+    double ans1, ans2;
+    cin >> a;
+
+    b = 100.0 - a;
+
+    ans1 = 100.0 / a;
+    ans2 = 100.0 / b;
+
+    cout << fixed << setprecision(10) << ans1 << "\n";
+    cout << fixed << setprecision(10) << ans2 << "\n";
 
     return 0;
 }
