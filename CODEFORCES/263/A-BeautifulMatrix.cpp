39d8b69f619d4d6ae2177b684eef96afe57f4149
AjnusAfter <darkskater360graus@gmail.com>
2026-04-08 12:21:34 -0300

    A: Beautiful Matrix solved

--- CODEFORCES/263/A-BeautifulMatrix.cpp
+++ CODEFORCES/263/A-BeautifulMatrix.cpp
@@ -15,5 +15,27 @@ int main()
 {
     _;
 
+    ll i_pos, j_pos;
+
+    vector<v64> mat(5, v64(5, 0));
+
+    forn(i, 0, 5)
+    {
+        forn(j, 0, 5)
+        {
+            cin >> mat[i][j];
+            if (mat[i][j] == 1)
+            {
+                i_pos = i + 1;
+                j_pos = j + 1;
+            }
+        }
+    }
+
+    // cout << i_pos << ln;
+    // cout << j_pos << ln;
+
+    cout << abs(3 - i_pos) + abs(3 - j_pos) << ln;
+
     return 0;
 }
