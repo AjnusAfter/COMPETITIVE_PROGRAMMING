584e013a7859e141cdd84499748ee9dff937f915
AjnusAfter <darkskater360graus@gmail.com>
2026-04-12 19:21:25 -0300

    A: Sliding Window Xor solved

--- VJudge/Lista 4 - Operações com Bits/A-SlidingWindowXor.cpp
+++ VJudge/Lista 4 - Operações com Bits/A-SlidingWindowXor.cpp
@@ -15,7 +15,31 @@ int main()
 {
     _;
 
-    
+    ll n, k, x, a, b, c;
+    cin >> n >> k >> x >> a >> b >> c;
+
+    ll windows = n - k + 1;
+    ll the_xor = 0;
+    ll cur = x;
+
+    forn(i, 1, n + 1)
+    {
+        ll left = max(1LL, i - k + 1);
+        ll right = min(i, windows);
+        ll cnt = max(0LL, right - left + 1);
+
+        if (cnt % 2 == 1)
+        {
+            the_xor ^= cur;
+        }
+
+        if (i < n)
+        {
+            cur = (a * cur + b) % c;
+        }
+    }
+
+    cout << the_xor << '\n';
 
     return 0;
 }
