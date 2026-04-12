4438295cbfdf88b43ae6bd5c060c466498d0d82f
AjnusAfter <darkskater360graus@gmail.com>
2026-04-12 15:08:57 -0300

    'lvable' solved

--- Kattis/lv-able/lv-able.cpp
+++ Kattis/lv-able/lv-able.cpp
@@ -9,12 +9,50 @@ typedef pair<ll, ll> p64;
     ios::sync_with_stdio(0); \
     cin.tie(0)
 #define forn(i, s, e) for (ll i = (s); i < (e); i++)
-#define fornm(i, s, e) for (ll i = (s); i < (e); ++i)
 #define ln "\n"
 
 int main()
 {
     _;
+
+    ll N;
+    string s;
+    // s.reserve(N);
+    cin >> N >> s;
+
+    forn(i, 0, N)
+    {
+        if (s[i] == 'l' && s[i + 1] == 'v')
+        {
+            cout << 0 << ln;
+            return 0;
+        }
+    }
+
+    bool has_l = 0;
+    bool has_v = 0;
+
+    for (char c : s)
+    {
+        if (c == 'l')
+        {
+            has_l = 1;
+        }
+
+        if (c == 'v')
+        {
+            has_v = 1;
+        }
+    }
+
+    if (has_l || has_v)
+    {
+        cout << 1 << ln;
+    }
+    else
+    {
+        cout << 2 << ln;
+    }
 
     return 0;
 }
