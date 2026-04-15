ffcac9659a57c0b93043222863f3fe5784bd46ba
AjnusAfter <darkskater360graus@gmail.com>
2026-04-14 22:36:47 -0300

    B: Bitwise Operators solved

--- VJudge/Lista 4 - Operações com Bits/B-BitwiseOperators.cpp
+++ VJudge/Lista 4 - Operações com Bits/B-BitwiseOperators.cpp
@@ -7,7 +7,7 @@ typedef pair<ll, ll> p64;
 
 #define _                    \
     ios::sync_with_stdio(0); \
-    cin.tie()
+    cin.tie(0)
 #define forn(i, s, e) for (ll i = (s); i < (e); i++)
 #define ln "\n"
 
@@ -15,7 +15,30 @@ int main()
 {
     _;
 
+    ll t;
+    cin >> t;
 
-    
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        ll a = 0, b = 0;
+
+        forn(i, 1, n + 1)
+        {
+            ll temp = n ^ i;
+            if (i <= temp && temp <= n)
+            {
+                a = i;
+                b = temp;
+                break;
+            }
+        }
+
+        if (a != 0 && b != 0)
+            cout << a << " " << b << ln;
+        else
+            cout << -1 << ln;
+    }
     return 0;
 }
