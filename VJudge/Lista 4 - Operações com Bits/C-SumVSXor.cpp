a85551390eee37703a63f03dc8db15098fadc99e
AjnusAfter <darkskater360graus@gmail.com>
2026-04-16 00:32:09 -0300

    C: Sum VS Xor solved

--- VJudge/Lista 4 - Operações com Bits/C-SumVSXor.cpp
+++ VJudge/Lista 4 - Operações com Bits/C-SumVSXor.cpp
@@ -11,11 +11,59 @@ typedef pair<ll, ll> p64;
 #define forn(i, s, e) for (ll i = (s); i < (e); i++)
 #define ln "\n"
 
+ll sum_xor(ll n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+
+    ll zero_bits = 0;
+
+    while (n > 0)
+    {
+        if ((n & 1) == 0)
+        {
+            zero_bits++;
+        }
+
+        n = n >> 1;
+    }
+
+    return 1L << zero_bits;
+
+    /*
+    // cout << "entrou n: " << n << ln << ln;
+    ll x = 0;
+
+    ll i = 0;
+    do
+    {
+        // cout << "n: " << n << "; " << "x: " << x << ln;
+
+        // cout << "comparando " << n << " + " << i << " == " << n << " ^ " << i << ln;
+        // cout << n + i << " | " << (n ^ i) << ln;
+        if (n + i == (n ^ i))
+        {
+            x++;
+        }
+
+        i++;
+    } while (i < n);
+
+
+    return x;
+    */
+}
+
 int main()
 {
     _;
 
-    
+    ll n;
+    cin >> n;
+
+    cout << sum_xor(n) << ln;
 
     return 0;
 }
