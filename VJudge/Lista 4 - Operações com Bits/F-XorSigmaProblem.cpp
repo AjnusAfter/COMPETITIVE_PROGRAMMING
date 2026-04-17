0ebb8811059bd1a1c10c1b8918d921ba4e582abb
AjnusAfter <darkskater360graus@gmail.com>
2026-04-17 09:50:55 -0300

    F: Xor Sigma Problem solved

--- VJudge/Lista 4 - Operações com Bits/F-XorSigmaProblem.cpp
+++ VJudge/Lista 4 - Operações com Bits/F-XorSigmaProblem.cpp
@@ -15,7 +15,50 @@ int main()
 {
     _;
 
+    _;
 
+    ll N;
+    cin >> N;
+
+    v64 a(N);
+    forn(i, 0, N)
+    {
+        cin >> a[i];
+    }
+
+    v64 pref(N + 1, 0);
+    forn(i, 1, N + 1)
+    {
+        pref[i] = pref[i - 1] ^ a[i - 1];
+    }
+
+    ll ans = 0;
+
+    forn(bit, 0, 31)
+    {
+        ll count_1 = 0;
+
+        forn(i, 0, N + 1)
+        {
+            if ((pref[i] >> bit) & 1LL)
+            {
+                count_1++;
+            }
+        }
+
+        ll count_0 = (N + 1) - count_1;
+        ans += count_0 * count_1 * (1LL << bit);
+    }
+
+    ll sum_A = 0;
+    forn(i, 0, N)
+    {
+        sum_A += a[i];
+    }
+
+    ans -= sum_A;
+
+    cout << ans << ln;
 
     return 0;
 }
