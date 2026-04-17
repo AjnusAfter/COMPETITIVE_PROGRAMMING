9433b69e56144a0eda4879f44ac79d19f2fd596d
AjnusAfter <darkskater360graus@gmail.com>
2026-04-17 06:54:38 -0300

    D: Red Scarf solved

--- VJudge/Lista 4 - Operações com Bits/D-RedScarf.cpp
+++ VJudge/Lista 4 - Operações com Bits/D-RedScarf.cpp
@@ -14,8 +14,35 @@ typedef pair<ll, ll> p64;
 int main()
 {
     _;
+    ll n;
+    cin >> n;
 
-    
+    if (n % 2 != 0)
+    {
+        cout << "n deve ser PAR" << ln;
+        return 0;
+    }
+
+    v64 a(n);
+    v64 v(n);
+    forn(i, 0, n)
+    {
+        cin >> a[i];
+    }
+
+    ll sum = 0;
+    forn(i, 0, n)
+    {
+        sum ^= a[i];
+    }
+
+    forn(i, 0, n)
+    {
+        v[i] = sum ^ a[i];
+        cout << v[i] << " ";
+    }
+
+    cout << ln;
 
     return 0;
 }
