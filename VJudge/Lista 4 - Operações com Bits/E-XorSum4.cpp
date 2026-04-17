b0c543f8cf041320f8ef776b4aebe79365edb4f0
AjnusAfter <darkskater360graus@gmail.com>
2026-04-17 09:37:43 -0300

    E: Xor Sum 4 solved

--- VJudge/Lista 4 - Operações com Bits/E-XorSum4.cpp
+++ VJudge/Lista 4 - Operações com Bits/E-XorSum4.cpp
@@ -11,11 +11,46 @@ typedef pair<ll, ll> p64;
 #define forn(i, s, e) for (ll i = (s); i < (e); i++)
 #define ln "\n"
 
+const ll MOD = 1000000007;
+
 int main()
 {
     _;
 
-    
+    ll N;
+    cin >> N;
+
+    v64 a(N);
+
+    forn(i, 0, N)
+    {
+        cin >> a[i];
+    }
+
+    ll ans = 0;
+    ll pow_2 = 1;
+
+    forn(bit, 0, 60)
+    {
+        ll count_1 = 0;
+
+        forn(i, 0, N)
+        {
+            if ((a[i] >> bit) & 1LL)
+            {
+                count_1++;
+            }
+        }
+
+        ll cnt0 = N - count_1;
+
+        ll contrib = (count_1 % MOD) * (cnt0 % MOD) % MOD;
+        contrib = (contrib * pow_2) % MOD;
+
+        ans = (ans + contrib) % MOD;
+        pow_2 = (pow_2 * 2) % MOD;
+    }
+    cout << ans << ln;
 
     return 0;
 }
