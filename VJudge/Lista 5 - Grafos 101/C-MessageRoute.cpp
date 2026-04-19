b3e2427a4b7c892f18871f554f368b7bdadaaafa
AjnusAfter <darkskater360graus@gmail.com>
2026-04-19 02:50:14 -0300

    set C: Message Route

---
+++ VJudge/Lista 5 - Grafos 101/C-MessageRoute.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> v64;
+typedef pair<ll, ll> p64;
+
+#define _                    \
+    ios::sync_with_stdio(0); \
+    cin.tie()
+#define forn(i, s, e) for (ll i = (s); i < (e); i++)
+#define ln "\n"
+
+// Disjoint Set Union (Union-Find)
+//
+// Supports find with path compression and union by size to maintain dynamic connectivity of disjoint sets.
+//
+// complexity: O(alpha(N)) amortized per op, O(N)
+
+struct dsu
+{
+    v64 id, len;
+
+    dsu(ll n) : id(n), len(n, 1) { iota(id.begin(), id.end(), 0); }
+
+    ll find(ll a) { return a == id[a] ? a : id[a] = find(id[a]); }
+
+    bool uni(ll a, ll b)
+    {
+        a = find(a), b = find(b);
+        if (a == b)
+            return false;
+        if (len[a] < len[b])
+            swap(a, b);
+        len[a] += len[b], id[b] = a;
+        return true;
+    }
+};
+
+void dfs(ll u, vector<v64> &g, v64 &vis)
+{
+    vis[u] = 1;
+    for (ll v : g[u])
+        if (!vis[v])
+            dfs(v, g, vis);
+}
+
+int main()
+{
+    _;
+
+    vector<v64> g;
+    vector<ll> vis;
+
+    return 0;
+}
