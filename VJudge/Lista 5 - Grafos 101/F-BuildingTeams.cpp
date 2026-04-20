fface226510df0578c6201dd2d34115c4206ab15
AjnusAfter <darkskater360graus@gmail.com>
2026-04-19 22:44:41 -0300

    F: Building Teams solved

--- VJudge/Lista 5 - Grafos 101/F-BuildingTeams.cpp
+++ VJudge/Lista 5 - Grafos 101/F-BuildingTeams.cpp
@@ -11,10 +11,66 @@ typedef pair<ll, ll> p64;
 #define forn(i, s, e) for (ll i = (s); i < (e); i++)
 #define ln "\n"
 
+bool dfs(ll u, ll color, vector<v64> &g, v64 &team)
+{
+    team[u] = color;
+
+    for (ll v : g[u])
+    {
+        if (!team[v])
+        {
+            if (!dfs(v, 3 - color, g, team))
+                return false;
+        }
+        else if (team[v] == team[u])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 int main()
 {
     _;
+
+    ll n, m;
+    cin >> n >> m;
+
+    vector<v64> g(n);
+    forn(i, 0, m)
+    {
+        ll a, b;
+        cin >> a >> b;
+
+        a--, b--;
+
+        g[a].push_back(b);
+        g[b].push_back(a);
+    }
+
+    v64 team(n, 0);
+
+    forn(i, 0, n)
+    {
+        if (!team[i])
+        {
+            if (!dfs(i, 1, g, team))
+            {
+                cout << "IMPOSSIBLE" << ln;
+                return 0;
+            }
+        }
+    }
+
+    forn(i, 0, n)
+    {
+        cout
+            << team[i] << ' ';
+    }
+
+    cout << ln;
 
     return 0;
 }
