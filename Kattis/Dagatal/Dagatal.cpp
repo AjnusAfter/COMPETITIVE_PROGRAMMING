ea23e2491e6014819da484b4898348fa905ce132
AjnusAfter <darkskater360graus@gmail.com>
2026-04-23 17:53:51 -0300

    Dagatal solved

--- Kattis/Dagatal/Dagatal.cpp
+++ Kattis/Dagatal/Dagatal.cpp
@@ -11,10 +11,35 @@ typedef pair<ll, ll> p64;
 #define forn(i, s, e) for (ll i = (s); i < (e); i++)
 #define ln "\n"
 
-
 int main()
 {
     _;
+
+    ll m;
+    cin >> m;
+
+    if (m < 8)
+    {
+        if (m % 2 == 1)
+        {
+            cout << 31 << ln;
+        }
+        else
+        {
+            cout << (m == 2 ? 28 : 30) << ln;
+        }
+    }
+    else
+    {
+        if (m % 2 == 0)
+        {
+            cout << 31 << ln;
+        }
+        else
+        {
+            cout << 30 << ln;
+        }
+    }
 
     return 0;
 }
