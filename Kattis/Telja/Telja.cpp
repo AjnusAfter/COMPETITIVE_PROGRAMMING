a4e43d71fd114f5e8ccce39a9ed854edb85a0697
AjnusAfter <darkskater360graus@gmail.com>
2026-04-25 22:25:44 -0300

    'Telja': Fastest Solution

--- Kattis/Telja/Telja.cpp
+++ Kattis/Telja/Telja.cpp
@@ -1,27 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef vector<ll> v64;
-typedef pair<ll, ll> p64;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-#define _                    \
-    ios::sync_with_stdio(0); \
-    cin.tie()
-#define forn(i, s, e) for (ll i = (s); i < (e); i++)
-#define ln "\n"
-
-int main()
-{
-    _;
-
-    ll n;
+    int n;
     cin >> n;
 
-    forn(i, 1, n+1)
-    {
-        cout << i << ln;
+    string out;
+    for (int i = 1; i <= n; i++) {
+        out += to_string(i) + '\n';
     }
 
+    cout << out;
     return 0;
 }
