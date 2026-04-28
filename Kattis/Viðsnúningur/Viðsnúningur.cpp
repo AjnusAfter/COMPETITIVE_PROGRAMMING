935ed5bf5e7e13f64e4100a3fb909a33d2eeba5a
AjnusAfter <darkskater360graus@gmail.com>
2026-04-28 06:12:19 -0300

    Viðsnúningur solved

--- Kattis/Viðsnúningur/Viðsnúningur.cpp
+++ Kattis/Viðsnúningur/Viðsnúningur.cpp
@@ -15,6 +15,17 @@ int main()
 {
     _;
 
+    string s;
+    cin >> s;
+
+    forn(i, 0, s.size() / 2)
+    {
+        char temp = s[i];
+        s[i] = s[s.size() - 1 - i];
+        s[s.size() - 1 - i] = temp;
+    }
+
+    cout << s << ln;
 
     return 0;
 }
