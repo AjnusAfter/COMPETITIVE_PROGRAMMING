8a9112224f214077b9bc38180f167202807b9e5b
AjnusAfter <darkskater360graus@gmail.com>
2026-04-28 17:22:03 -0300

    A: Anton and Dankin solved

--- CODEFORCES/734/A-AntonAndDanik.cpp
+++ CODEFORCES/734/A-AntonAndDanik.cpp
@@ -15,7 +15,41 @@ int main()
 {
     _;
 
-    
+    ll n;
+    cin >> n;
+
+    ll a_count = 0, d_count = 0;
+    string ans;
+
+    forn(i, 0, n)
+    {
+        char c;
+        cin >> c;
+
+        if (c == 'A')
+        {
+            a_count++;
+        }
+        else
+        {
+            d_count++;
+        }
+    }
+
+    if (a_count > d_count)
+    {
+        ans = "Anton";
+    }
+    else if (d_count > a_count)
+    {
+        ans = "Danik";
+    }
+    else
+    {
+        ans = "Friendship";
+    }
+
+    cout << ans << ln;
 
     return 0;
 }
