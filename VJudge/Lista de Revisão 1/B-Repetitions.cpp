ff715065b637984664bc59c3258b23c49c259ba5
AjnusAfter <darkskater360graus@gmail.com>
2026-04-30 16:27:26 -0300

    B: Repetitions solved

--- VJudge/Lista de Revisão 1/B-Repetitions.cpp
+++ VJudge/Lista de Revisão 1/B-Repetitions.cpp
@@ -15,7 +15,42 @@ int main()
 {
     _;
 
-    
+    string s;
+    cin >> s;
+
+    // ll a_count = 0, c_count = 0, g_count = 0, t_count = 0;
+    /*string a_pilha, c_pilha, g_pilha, t_pilha;
+
+    forn(i, 0, s.size())
+    {
+        if ((s[i] == 'A' && a_pilha.size() == 0) || (s[i] == 'A' && s[i - 1] == 'A'))
+            a_pilha.push_back(s[i]);
+        else if ((s[i] == 'C' && c_pilha.size() == 0) || (s[i] == 'C' && s[i - 1] == 'C'))
+            c_pilha.push_back(s[i]);
+        else if ((s[i] == 'G' && g_pilha.size() == 0) || (s[i] == 'G' && s[i - 1] == 'G'))
+            g_pilha.push_back(s[i]);
+        else if ((s[i] == 'T' && t_pilha.size() == 0) || (s[i] == 'T' && s[i - 1] == 'T'))
+            t_pilha.push_back(s[i]);
+    }
+
+    cout << max({a_pilha.size(), c_pilha.size(), g_pilha.size(), t_pilha.size()}) << ln;
+    */
+
+    ll best = 1, current = 1;
+
+    forn(i, 1, s.size())
+    {
+        if (s[i] == s[i - 1])
+            current++;
+        else
+        {
+            current = 1;
+        }
+
+        best = max(best, current);
+    }
+
+    cout << best << ln;
 
     return 0;
 }
