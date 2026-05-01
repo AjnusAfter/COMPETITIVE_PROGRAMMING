a7080e785f51e8ac41e1f8e794c4ffabe12a3491
AjnusAfter <darkskater360graus@gmail.com>
2026-05-01 15:07:44 -0300

    C: Buy An Integer solved

--- VJudge/Lista de Revisão 1/C-BuyAnInteger.cpp
+++ VJudge/Lista de Revisão 1/C-BuyAnInteger.cpp
@@ -15,7 +15,38 @@ int main()
 {
     _;
 
-    
+    ll A, B, X;
+    cin >> A >> B >> X;
+
+    ll l = 0, r = 1e9 + 1;
+
+    while (r - l > 1)
+    {
+        ll mid = (l + r) / 2;
+
+        if (A * mid + B * to_string(mid).size() <= X)
+        {
+            l = mid;
+        }
+        else
+        {
+            r = mid;
+        }
+    }
+
+    cout << l << ln;
+
+    /*
+    // 10 x 9 + 7 × 1 = 97
+    A *
+                N +
+            B *d(N) <=
+        X;
+    A *N <= X - B *d(N);
+    N <= (X - B * d(N)) / A;
+
+    d(N) < d(X)
+    */
 
     return 0;
 }
