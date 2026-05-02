59c92dfc3802e5f4cf27714ff0c39d9c012220cc
AjnusAfter <darkskater360graus@gmail.com>
2026-05-02 13:38:46 -0300

    D: Reindeer and Sleigh solved

--- VJudge/Lista de Revisão 1/D-ReindeerAndSleigh.cpp
+++ VJudge/Lista de Revisão 1/D-ReindeerAndSleigh.cpp
@@ -7,7 +7,7 @@ typedef pair<ll, ll> p64;
 
 #define _                    \
     ios::sync_with_stdio(0); \
-    cin.tie()
+    cin.tie(0)
 #define forn(i, s, e) for (ll i = (s); i < (e); i++)
 #define ln "\n"
 
@@ -15,7 +15,25 @@ int main()
 {
     _;
 
-    
+    ll N, Q;
+    cin >> N >> Q;
+
+    v64 R(N) /*, v2(Q)*/;
+    forn(i, 0, N) cin >> R[i];
+    // forn(i, 0, Q) cin >> v2[i];
+
+    sort(R.begin(), R.end());
+
+    v64 prefix(N + 1, 0);
+    forn(i, 0, N) prefix[i + 1] = prefix[i] + R[i];
+
+    while (Q--)
+    {
+        ll X;
+        cin >> X;
+
+        cout << upper_bound(prefix.begin(), prefix.end(), X) - prefix.begin() - 1 << ln;
+    }
 
     return 0;
 }
