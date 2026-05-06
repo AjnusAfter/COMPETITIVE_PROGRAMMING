529a188474ea278749e627708f12f4bcda9ff383
AjnusAfter <darkskater360graus@gmail.com>
2026-05-06 18:26:28 -0300

    G: Xorry1 solved

--- VJudge/Lista de Revisão 1/G-Xorry1.cpp
+++ VJudge/Lista de Revisão 1/G-Xorry1.cpp
@@ -15,7 +15,37 @@ int main()
 {
     _;
 
-    
+    ll T;
+    cin >> T;
+
+    while (T--)
+    {
+        ll X;
+        cin >> X;
+
+        ll msb = 1;
+        while ((msb << 1) <= X)
+        {
+            msb <<= 1;
+        }
+
+        ll A = X ^ msb;
+        ll B = msb;
+
+        cout << A << " " << B << ln;
+
+        /*ll l = 0;
+        forn(r, 0, X)
+        {
+            if ((l ^ r) == X)
+            {
+                cout << l << " " << r << ln;
+            }
+
+            l++;
+        }
+            */
+    }
 
     return 0;
 }
