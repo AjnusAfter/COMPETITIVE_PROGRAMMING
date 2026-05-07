d10618e9f305a6b71d5739870f6b199183e606ce
AjnusAfter <darkskater360graus@gmail.com>
2026-05-07 04:20:57 -0300

    H: Bitwise Tuples solved

--- VJudge/Lista de Revisão 1/H-BitwiseTuples.cpp
+++ VJudge/Lista de Revisão 1/H-BitwiseTuples.cpp
@@ -11,11 +11,55 @@ typedef pair<ll, ll> p64;
 #define forn(i, s, e) for (ll i = (s); i < (e); i++)
 #define ln "\n"
 
+const ll MOD = 1e9 + 7;
+
 int main()
 {
     _;
 
-    
+    ll T;
+    cin >> T;
+
+    while (T--)
+    {
+        ll N, M;
+        cin >> N >> M;
+
+        // calcula (2^N) % MOD | cmath pow() perde precisão; estoura; não funciona com módulo; inutilizável para Prog Comp
+        ll base = 2;
+        ll exp = N;
+        ll pow2 = 1;
+
+        while (exp)
+        {
+            if (exp & 1)
+            {
+                pow2 = (pow2 * base) % MOD;
+            }
+
+            base = (base * base) % MOD;
+            exp >>= 1;
+        }
+
+        ll x = (pow2 - 1 + MOD) % MOD; // (2^N-1) % MOD;
+
+        // calcula (x^M) % MOD
+        base = x;
+        exp = M;
+
+        ll ans = 1;
+
+        while (exp)
+        {
+            if (exp & 1)
+                ans = (ans * base) % MOD;
+
+            base = (base * base) % MOD;
+            exp >>= 1;
+        }
+
+        cout << ans << ln;
+    }
 
     return 0;
 }
