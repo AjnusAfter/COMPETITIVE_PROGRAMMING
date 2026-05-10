e8c0c1b6e8084e8114367edf8b4047fe2e486353
AjnusAfter <darkskater360graus@gmail.com>
2026-05-10 17:00:54 -0300

    Pascal's Hyper-Pyramids solved

--- Kattis/Pascal's Hyper-Pyramids/PascalsHyperPyramids.cpp
+++ Kattis/Pascal's Hyper-Pyramids/PascalsHyperPyramids.cpp
@@ -12,11 +12,65 @@ typedef pair<ll, ll> p64;
 #define ln "\n"
 
 
+ll C(ll n, ll k)
+{
+    k = min(k, n - k);
+
+    __int128 ans = 1;
+
+    forn(i, 1, k + 1)
+        ans = ans * (n - k + i) / i;
+
+    return (ll)ans;
+}
+
+ll multinomial(const vector<ll> &v)
+{
+    ll rem = accumulate(v.begin(), v.end(), 0LL);
+
+    __int128 ans = 1;
+
+    for (ll x : v)
+    {
+        ans *= C(rem, x);
+        rem -= x;
+    }
+
+    return (ll)ans;
+}
+
+void gen(ll idx, ll dims, ll rem, ll last, vector<ll> &v, set<ll> &ans)
+{
+    if (idx == dims - 1)
+    {
+        if (rem < last)
+            return;
+
+        v[idx] = rem;
+
+        ans.insert(multinomial(v));
+        return;
+    }
+
+    forn(x, last, rem + 1)
+    {
+        v[idx] = x;
+        gen(idx + 1, dims, rem - x, x, v, ans);
+    }
+}
+
 int main()
 {
     _;
 
-    
+    ll D, H;
+    cin >> D >> H;
 
-    return 0;
+    vector<ll> v(D, 0);
+    set<ll> ans;
+
+    gen(0, D, H - 1, 0, v, ans);
+
+    for (ll x : ans)
+        cout << x << ln;
 }
