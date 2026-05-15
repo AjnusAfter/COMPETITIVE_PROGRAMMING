3f614993c3c5d8c8197f3c2918f8c762a16ca39f
AjnusAfter <darkskater360graus@gmail.com>
2026-05-15 16:52:18 -0300

    Hackenbush Bamboo solved

--- Kattis/Hackenbush Bamboo/HackenbushBamboo.cpp
+++ Kattis/Hackenbush Bamboo/HackenbushBamboo.cpp
@@ -15,7 +15,21 @@ int main()
 {
     _;
 
-    
+    ll n;
+    cin >> n;
+
+    ll grundy = 0;
+
+    // v64 h(n);
+    forn(i, 0, n)
+    {
+        ll h;
+        cin >> h;
+
+        grundy ^= h;
+    }
+
+    cout << "*" << grundy << ln;
 
     return 0;
 }
