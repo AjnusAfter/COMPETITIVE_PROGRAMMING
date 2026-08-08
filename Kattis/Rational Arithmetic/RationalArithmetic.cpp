cf5115a112caeea859b6b5f706e412d5bb2e89df
AjnusAfter <darkskater360graus@gmail.com>
2026-08-08 03:11:17 -0300

    Rational Arithmetic solved (Fastest Solution)

--- Kattis/Rational Arithmetic/RationalArithmetic.cpp
+++ Kattis/Rational Arithmetic/RationalArithmetic.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <numeric>
 using namespace std;
 
 typedef long long ll;
@@ -15,7 +16,51 @@ int main()
 {
     _;
 
-    
+    ll t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll x1, y1, x2, y2, num, dem, div;
+        char op;
+
+        cin >> x1 >> y1 >> op >> x2 >> y2;
+
+        switch (op)
+        {
+        case '+':
+            num = x1 * y2 + x2 * y1;
+            dem = y1 * y2;
+            break;
+        case '-':
+            num = x1 * y2 - x2 * y1;
+            dem = y1 * y2;
+            break;
+        case '*':
+            num = x1 * x2;
+            dem = y1 * y2;
+            break;
+        case '/':
+            num = x1 * y2;
+            dem = y1 * x2;
+            break;
+        }
+
+        div = gcd(num, dem);
+        if (div > 1)
+        {
+            num /= div;
+            dem /= div;
+        }
+
+        if (dem < 0)
+        {
+            num = -num;
+            dem = -dem;
+        }
+
+        cout << num << " / " << dem << ln;
+    }
 
     return 0;
 }
