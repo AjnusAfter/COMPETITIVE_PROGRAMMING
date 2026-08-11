4b986f08bfc480d8c1530becd23749b75b5a490d
AjnusAfter <darkskater360graus@gmail.com>
2026-08-11 01:39:03 -0300

    116-A: Tram solved

--- CODEFORCES/116/A-Tram.cpp
+++ CODEFORCES/116/A-Tram.cpp
@@ -15,7 +15,22 @@ int main()
 {
     _;
 
-    
+    ll n;
+    cin >> n;
+
+    ll pass_at = 0;
+    ll pass_tot = 0;
+
+    forn(i, 0, n)
+    {
+        ll a, b;
+        cin >> a >> b;
+
+        pass_at += b - a;
+        pass_tot = max(pass_tot, pass_at);
+    }
+
+    cout << pass_tot << ln;
 
     return 0;
 }
