6fb05a908412cb26e7cb57ede8dbd78c4119b294
AjnusAfter <darkskater360graus@gmail.com>
2026-08-15 17:44:37 -0300

    Ballpark Estimate solved (Fastest Solution)

--- Kattis/Ballpark Estimate/Ballpark Estimate.cpp
+++ Kattis/Ballpark Estimate/Ballpark Estimate.cpp
@@ -16,7 +16,27 @@ int main()
 
     _;
 
-    
+    ll n;
+    cin >> n;
 
+    if (n < 10)
+    {
+        cout << n << ln;
+        return 0;
+    }
+
+    ll base = 1;
+    while (base * 10 <= n)
+    {
+        base *= 10;
+    }
+
+    ll pot_low = (n / base) * base;
+    ll pot_high = pot_low + base;
+
+    ll ans;
+    n - pot_low < pot_high - n ? ans = pot_low : ans = pot_high;
+
+    cout << ans << ln;
     return 0;
 }
