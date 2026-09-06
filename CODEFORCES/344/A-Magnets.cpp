0a179459f621ab54606dbab6316e6bea8d50acb3
AjnusAfter <darkskater360graus@gmail.com>
2026-09-06 02:16:54 -0300

    set 344-A: Magnets

---
+++ CODEFORCES/344/A-Magnets.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> v64;
+typedef pair<ll, ll> p64;
+
+#define _                    \
+    ios::sync_with_stdio(0); \
+    cin.tie(0)
+#define forn(i, s, e) for (ll i = (s); i < (e); i++)
+#define ln "\n"
+
+int main()
+{
+    _;
+
+
+
+    return 0;
+}
