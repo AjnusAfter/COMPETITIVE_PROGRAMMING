0ed8715250c0b1e1f3d81070ffa0f51d672917f1
AjnusAfter <darkskater360graus@gmail.com>
2026-09-06 22:37:12 -0300

    Toll Booths solved (Fastest Solution)

--- Kattis/Toll Booths/TollBooths.cpp
+++ Kattis/Toll Booths/TollBooths.cpp
@@ -15,7 +15,26 @@ int main()
 {
     _;
 
-    
+    ll n;
+    cin >> n;
+
+    ll saldo = 0, menor = 0;
+
+    forn(i, 0, n)
+    {
+        char c;
+        ll x;
+        cin >> c >> x;
+
+        if (c == 'T')
+            saldo -= x;
+        else
+            saldo += x;
+
+        menor = min(menor, saldo);
+    }
+
+    cout << -menor << ln;
 
     return 0;
 }
