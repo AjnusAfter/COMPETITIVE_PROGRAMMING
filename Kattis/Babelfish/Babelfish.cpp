9e5cad363605c22ac38cbaa8e262a5ee0feb447c
AjnusAfter <darkskater360graus@gmail.com>
2026-09-16 03:01:31 -0300

    Babelfish solved

--- Kattis/Babelfish/Babelfish.cpp
+++ Kattis/Babelfish/Babelfish.cpp
@@ -11,12 +11,43 @@ typedef pair<ll, ll> p64;
 #define forn(i, s, e) for (ll i = (s); i < (e); i++)
 #define ln "\n"
 
-
 int main()
 {
     _;
 
+    unordered_map<string, string> mp;
+    string line;
 
+    // read dict
+    while (getline(cin, line))
+    {
+        if (line.empty())
+        {
+            break;
+        }
+
+        stringstream ss(line);
+
+        string english, foreign;
+        ss >> english >> foreign;
+
+        mp[foreign] = english;
+    }
+
+    // read msg words
+    string word;
+    while (getline(cin, word))
+    {
+        if (mp.count(word))
+        {
+            cout << mp[word] << ln;
+        }
+
+        else
+        {
+            cout << "eh" << ln;
+        }
+    }
 
     return 0;
 }
